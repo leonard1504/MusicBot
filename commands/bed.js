63a86da301066bec9970339f6e036d455c91cc5f
leonard1504 <leonard.kipar@gmail.com>
2022-02-23 18:00:35 +0100

    added new picture functions

---
+++ commands/bed.js
@@ -0,0 +1,39 @@
+const {
+    SlashCommandBuilder
+} = require('@discordjs/builders');
+const {
+    MessageEmbed,
+    MessageAttachment
+} = require('discord.js');
+const {
+    color
+} = require('../config.json');
+const canvacord = require("canvacord");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('bed')
+        .addMentionableOption(option => option.setName('mention').setRequired(true).setDescription("Erwähne hier eine Person"))
+        .addMentionableOption(option => option.setName('mention2').setRequired(true).setDescription("Erwähne hier eine Person"))
+        .setDescription('Da ist ein Monster unter meinem Bett'),
+    async execute(interaction) {
+        if (interaction.member.nickname != null) {
+            nick = interaction.member.nickname;
+        } else {
+            nick = interaction.user.username;
+        }
+        userpp = interaction.user.avatarURL();
+        mentionedUser = interaction.options.get('mention').user.avatarURL({ dynamic: false, format: 'png' });
+        mentionedUser2 = interaction.options.get('mention2').user.avatarURL({ dynamic: false, format: 'png' });
+        image = await canvacord.Canvacord.bed(mentionedUser.toString(), mentionedUser2.toString());
+        let attachment = await new MessageAttachment(image, "bed.png");
+        const embedbed = new MessageEmbed()
+            .setColor(`${color}`)
+            .setImage('attachment://bed.png')
+            .setFooter(`Ausgeführt von:  ${nick}`, `${userpp}`);
+        await interaction.reply({
+            embeds: [embedbed],
+            files: [attachment]
+        });
+    },
+};
